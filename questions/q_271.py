39f142ff2326701649dd3b06fa278eaaf89fcb83
myst3l <karakumayl@gmail.com>
2023-05-14 16:43:55 -0400

    from mac

--- questions/q_271.py
+++ questions/q_271.py
@@ -1,30 +1,33 @@
 # Design an algorithm to encode a list of strings to a string.
 # The encoded string is then sent over the network and is decoded back to the original list of strings.
 # Q 659 on lintcode
-strs = ["lint","code","love","you"]
+strs = ["lintingverybig","code","love","you"]
 
 
 def encode(strs):
     encrypted = ""
     for i in range(0, len(strs)):
-        encrypted += strs[i] + ';:'
+        encrypted += str(len(strs[i])) +'#' +strs[i]
     return encrypted
 
 
 def decode(encrypted):
-    holder = ""
-    ans = []
-    words = 0
 
-    for i in range(0, len(encrypted)):
-        if encrypted[i] == ';':
-            if encrypted[i+1] == ':':
-                ans.append(holder)
-                holder = ""
-        elif encrypted[i] == ':':
-            words += 1
-        else:
-            holder += encrypted[i]
+    ans = []
+    while len(encrypted) > 0:
+        numberlength = 1
+        reached = False
+
+        while not reached:
+            if encrypted[numberlength] == '#':
+                reached = True
+            else:
+                numberlength += 1
+
+        wordlength = int(encrypted[0:numberlength])
+        ans.append(encrypted[numberlength+1:numberlength+1+wordlength])
+        encrypted = encrypted[numberlength + 1 + wordlength:]
+
     return ans
 
 
@@ -32,3 +35,29 @@ encr = encode(strs)
 print(encr)
 dec = decode(encr)
 print(dec)
+
+# def encode(strs):
+#     encrypted = ""
+#     for i in range(0, len(strs)):
+#         encrypted += strs[i] + ';:'
+#     return encrypted
+#
+#
+# def decode(encrypted):
+#     holder = ""
+#     ans = []
+#     words = 0
+#
+#     for i in range(0, len(encrypted)):
+#         if encrypted[i] == ';':
+#             if encrypted[i+1] == ':':
+#                 ans.append(holder)
+#                 holder = ""
+#         elif encrypted[i] == ':':
+#             words += 1
+#         else:
+#             holder += encrypted[i]
+#     return ans
+#
+#
+
